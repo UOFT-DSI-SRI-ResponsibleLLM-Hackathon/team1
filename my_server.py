f93f8a55650aea7eea4daffcd22e4734199ad08d
Haoran Zhao <haoran.zhao@mail.utoronto.ca>
2024-10-05 13:08:44 -0400

    solved security leak

--- my_server.py
+++ my_server.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 import json
 from LLMQuery import LLMQuery
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +13,9 @@ CORS(app)
 # Initialize your LLMQuery
 with open("./profiles.json", 'r') as agent_profiles_fp:
     agent_profiles = json.load(agent_profiles_fp)
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+groq_api_key = os.getenv("GROQ_API_KEY")
 
 use_groq = False
 
@@ -30,16 +36,37 @@ def query():
 
         # Call the LLMQuery's query method
         if use_groq:
-            groq_api_key = "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"
             api_base = "https://api.groq.com/openai/v1"
             llm = LLMQuery(groq_api_key, model="llama3-70b-8192", api_base=api_base)
         else:
-            openai_api_key = "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"
             llm = LLMQuery(openai_api_key, model="gpt-4o")
-        response = llm.query_with_retrieve(prompt)
+
+        response = llm.query(prompt)
         return jsonify({"response": response})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+@app.route('/llm-query/no-rag', methods=['POST'])
+def query_without_RAG():
+    try:
+        data = request.get_json()
+        prompt = data.get("message", "")
+        if not prompt:
+            return jsonify({"error": "No prompt provided"}), 400
+
+        # Call the LLMQuery's query method
+        if use_groq:
+            api_base = "https://api.groq.com/openai/v1"
+            llm = LLMQuery(groq_api_key, model="llama3-70b-8192", api_base=api_base)
+        else:
+            llm = LLMQuery(openai_api_key, model="gpt-4o")
+        
+        response = llm.query(prompt)
+        return jsonify({"response": response})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
